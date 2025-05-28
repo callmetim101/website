714cbb3b303ec90755ec3600fb8b6b61da8c3b4a
Tim Call <callmetim101@gmail.com>
2025-05-28 19:06:53 -0400

    react tutorial

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "/src/Header.jsx"
+import Footer from "/src/Footer.jsx"
+import Food from "/src/Food.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
