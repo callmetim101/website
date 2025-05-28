61cc4da727c61e5899f8137403443876d52ce566
Tim Call <callmetim101@gmail.com>
2025-05-28 19:09:16 -0400

    next lesson

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,9 @@
-import Header from "/src/Header.jsx"
-import Footer from "/src/Footer.jsx"
-import Food from "/src/Food.jsx"
+
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
     </>
   );
 }
